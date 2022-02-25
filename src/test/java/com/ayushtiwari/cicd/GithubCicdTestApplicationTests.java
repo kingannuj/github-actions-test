e3a127555842d3e2e874845d8a3dabcec54ef3b8
kingannuj <63520863+kingannuj@users.noreply.github.com>
2022-02-25 18:08:07 +0530

    code

---
+++ src/test/java/com/ayushtiwari/cicd/GithubCicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayushtiwari.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
